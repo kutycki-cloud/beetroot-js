cb15cbdeb770d1953e37aedb72114b02ef9b7f80
kutycki-cloud <74899889+kutycki-cloud@users.noreply.github.com>
2021-05-21 16:54:11 +0300

    new-repo

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// let admin, username;
+// let username = "Джон";
+// admin = username;
+
+// alert(admin);
+
+
+
+
+// let text = "hello";
+// let user = "Vasia";
+
+// alert(text + " " + user);
+
+
+
+
+
+
+// let user = prompt('Як тебе звати?');
+// alert(user);
+
+
+
+let number = prompt('Insert', 356)
+let num1 = (number % 100)
+
+console.log(num1)
+
